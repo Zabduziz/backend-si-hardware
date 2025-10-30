c9c3e3605bc110ff3e259b0f4726953233f36a16
Zaidan Abdul Aziz <zaidanabdulaziz03@gmail.com>
2025-10-30 11:42:13 +0700

    feat: add function and add kelasModel

--- src/migrations/20251028145600-create-kelas-table.js
+++ src/migrations/20251028145600-create-kelas-table.js
@@ -2,32 +2,33 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('kelas-table', {
-      idKelas: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        primaryKey: true
-      },
-      namaKelas: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      }
-    })
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('kelas-table', {
+            idKelas: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+                primaryKey: true
+            },
+            namaKelas: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+            }
+        })
+    },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('kelas-table')
-  }
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('kelas-table')
+    }
 };
