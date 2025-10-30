9d7f8dd5b71d18f68b8da112212891ac86a8ccee
Zaidan Abdul Aziz <zaidanabdulaziz03@gmail.com>
2025-10-30 11:27:25 +0700

    fix: table history key to dosen and kelas

--- src/migrations/20251028153316-create-historykegiatan-table.js
+++ src/migrations/20251028153316-create-historykegiatan-table.js
@@ -2,95 +2,95 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('historykegiatan-table', {
-      idHistory: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-        unique: true,
-        primaryKey: true
-      },
-      idUser: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        references: {
-          model: 'user-table',
-          key: 'idUser'
-        },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-      },
-      idPraktikum: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        references: {
-          model: 'tipepraktikum-table',
-          key: 'idPraktikum'
-        },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-      },
-      idLab: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        references: {
-          model: 'ruanglab-table',
-          key: 'idLab'
-        },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-      },
-      idDosen: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        references: {
-          model: 'dosen-table',
-          key: 'idDosen'
-        },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-      },
-      idKelas: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        references: {
-          model: 'kelas-table',
-          key: 'idKelas'
-        },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-      },
-      tanggal: {
-        type: Sequelize.DATEONLY,
-        allowNull: false,
-      },
-      waktu: {
-        type: Sequelize.TIME,
-        allowNull: false,
-      },
-      tindakLanjut: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      ttd: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      }
-    })
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('historykegiatan-table', {
+            idHistory: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                allowNull: false,
+                unique: true,
+                primaryKey: true
+            },
+            idUser: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                references: {
+                    model: 'user-table',
+                    key: 'idUser'
+                },
+                onDelete: 'CASCADE',
+                onUpdate: 'CASCADE'
+            },
+            idPraktikum: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                references: {
+                    model: 'tipepraktikum-table',
+                    key: 'idPraktikum'
+                },
+                onDelete: 'CASCADE',
+                onUpdate: 'CASCADE'
+            },
+            idLab: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                references: {
+                    model: 'ruanglab-table',
+                    key: 'idLab'
+                },
+                onDelete: 'CASCADE',
+                onUpdate: 'CASCADE'
+            },
+            idDosen: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                references: {
+                    model: 'dosen-table',
+                    key: 'idDosen'
+                },
+                onDelete: 'CASCADE',
+                onUpdate: 'CASCADE'
+            },
+            idKelas: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                references: {
+                    model: 'kelas-table',
+                    key: 'idKelas'
+                },
+                onDelete: 'CASCADE',
+                onUpdate: 'CASCADE'
+            },
+            tanggal: {
+                type: Sequelize.DATEONLY,
+                allowNull: false,
+            },
+            waktu: {
+                type: Sequelize.TIME,
+                allowNull: false,
+            },
+            tindakLanjut: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            ttd: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+            }
+        })
+    },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('historykegiatan-table')
-  }
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('historykegiatan-table')
+    }
 };
