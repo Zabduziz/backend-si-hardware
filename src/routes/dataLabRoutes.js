faad5b20ec4ab71fa5f3fca07c6898f23e66fd43
Zaidan Abdul Aziz <zaidanabdulaziz03@gmail.com>
2025-11-01 22:10:33 +0700

    feat: can add barang to lab each

--- src/routes/dataLabRoutes.js
+++ src/routes/dataLabRoutes.js
@@ -7,8 +7,5 @@ const dataLabController = require('../controllers/dataLabController')
 router.put('/updateDataLab/:idLab', verifyToken, dataLabController.updateDataLab)
 router.get('/getDataLab/:idLab', dataLabController.getAllDataLab)
 router.post('/addDataLab', verifyToken, dataLabController.addDataLab)
-//TODO: Buatkan add barang nya satu-satu jangan langsung masukin ke lab
-//TODO: Buatkan terlebih dahulu barangnya nanti biar gampang dimasukkin
-//TODO: CRUD Biasa pada umumnya
 
 module.exports = router
